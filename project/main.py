1632b07ebe4c10f00b538932ff4ea1bf3bb4d13f
junboom <ticktock0414@gmail.com>
2025-08-21 00:41:52 +0900

    [fix] project
    Signed-off-by: junboom <ticktock0414@gmail.com>

--- project/main.py
+++ project/main.py
@@ -1,17 +1,12 @@
 import os
-import sys
 import io
 import math
-import numpy as np
-import torch
 import pandas as pd
-import json
 import streamlit as st
-import tempfile
 import fitz  # PyMuPDF
 
 from PIL import Image
-from peft import LoraConfig, PeftModel, get_peft_model, prepare_model_for_kbit_training, TaskType
+from peft import LoraConfig, PeftModel, get_peft_model, TaskType
 from datasets import Dataset
 from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, TrainerCallback
@@ -24,16 +19,15 @@ from langchain_huggingface import HuggingFaceEmbeddings, HuggingFacePipeline
 from langchain.text_splitter import CharacterTextSplitter
 from langgraph.graph import StateGraph, END
 
-os.environ['LANGCHAIN_TRACING_V2']='true'
-os.environ['LANGCHAIN_ENDPOINT']='https://api.smith.langchain.com'
-os.environ['LANGCHAIN_API_KEY']='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'
-os.environ['LANGCHAIN_PROJECT']='pr-shadowy-scow-41'
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
+os.environ['LANGCHAIN_API_KEY'] = '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'
+os.environ['LANGCHAIN_PROJECT'] = 'pr-shadowy-scow-41'
 
 # ----------------------
 # 설정
 # ----------------------
 MODEL_ID = 'torchtorchkimtorch/Llama-3.2-Korean-GGACHI-1B-Instruct-v1'
-# MODEL_ID = 'tunib/electra-ko-base'
 EMBED_MODEL_NAME = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
 
 LORA_DIR = 'lora_model'
@@ -49,7 +43,8 @@ OVERLAP = 128
 
 PAGE_TITLE = 'Car Manual QA'
 
-TEMPLATE = '자동차 매뉴얼 문서를 바탕으로 질문에 답해주세요.\n문서 내용:\n{context}\n\n질문:\n{query}\n\n답변:\n'
+TEMPLATE = '문서 내용을 바탕으로 질문에 답변해주세요.\n문서 내용:\n{context}\n\n질문:\n{query}\n\n답변:\n'
+SUMMARY_TEMPLATE = '문서 내용을 바탕으로 요약해주세요.\n문서 내용:\n{context}\n\n요약:\n'
 
 st.set_page_config(page_title=PAGE_TITLE, layout='wide')
 
@@ -85,6 +80,21 @@ def chunk_text(texts):
 def load_csv(csv_path):
     df = pd.read_csv(csv_path)
 
+    supply_map = {
+        'engine_oil': '엔진 오일',
+        'oil_filter': '오일 필터',
+        'air_filter': '에어 필터',
+        'cabin_air_filter': '에어컨 필터',
+        'front_brake_pads': '앞 브레이크 패드',
+        'rear_brake_pads': '뒤 브레이크 패드',
+        'coolant': '냉각수',
+        'wiper_blades': '와이퍼 블레이드',
+        'spark_plugs': '스파크 플러그',
+        'tires': '타이어',
+        'battery': '배터리',
+        'transmission_fluid': '미션 오일'
+    }
+
     status_map = {
         -1: '즉시 교체가 필요합니다.',
         0: '곧 교체가 필요할 것 같습니다.',
@@ -94,13 +104,13 @@ def load_csv(csv_path):
     texts = []
     for _, row in df.iterrows():
         text = (
-            f'자동차 소모품: {row["items"]}\n'
-            f'현재 주행거리: {row["mileage_km"]} km\n'
-            f'현재까지 사용한 개월 수: {row["mileage_month"]}개월\n'
-            f'교체 기준 거리: {row["replacement_km"]} km\n'
-            f'교체 기준 개월: {row["relacement_month"]}개월\n'
-            f'현재 부품의 사용량: {row["used_percent"]}%\n'
-            f'교체 시기: {status_map[row["status"]]}'
+            f'내 차의 소모품: {supply_map[row["items"]]}\n'
+            f'내 차의 해당 소모품을 사용하여 주행한 거리: {row["mileage_km"]} km\n'
+            f'내 차의 해당 소모품을 사용한 개월 수: {row["mileage_month"]}개월\n'
+            f'해당 소모품을 교체해야 하는 주행거리: {row["replacement_km"]} km\n'
+            f'해당 소모품을 교체해야 하는 개월 수: {row["relacement_month"]}개월\n'
+            f'내 차의 해당 소모품의 사용량: {row["used_percent"]}%\n'
+            f'내 차의 해당 소모품의 교체 시기: {status_map[row["status"]]}'
         )
         texts.append(text)
 
@@ -111,8 +121,6 @@ def lora_fine_tuning(base_model, tokenizer, chunks):
     train_dataset = Dataset.from_dict({'text': train_texts})
     eval_dataset = Dataset.from_dict({'text': eval_texts})
 
-    prepared_model = prepare_model_for_kbit_training(base_model)
-
     lora_config = LoraConfig(
         r=8,
         lora_alpha=32,
@@ -121,7 +129,7 @@ def lora_fine_tuning(base_model, tokenizer, chunks):
         bias='none',
         task_type=TaskType.CAUSAL_LM
     )
-    model = get_peft_model(prepared_model, lora_config)
+    model = get_peft_model(base_model, lora_config)
 
     def preprocess(data):
         tokenized = tokenizer(
@@ -142,7 +150,7 @@ def lora_fine_tuning(base_model, tokenizer, chunks):
         output_dir=LORA_DIR,
         per_device_train_batch_size=4,
         per_device_eval_batch_size=4,
-        num_train_epochs=10,
+        num_train_epochs=3,
         eval_strategy='epoch',
         save_strategy='epoch',
         load_best_model_at_end=True,
@@ -177,21 +185,22 @@ def lora_fine_tuning(base_model, tokenizer, chunks):
 # ----------------------
 # PDF 처리
 # ----------------------
+@st.cache_data
 def load_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
+    context = fitz.open(pdf_path)
     texts = []
     page_images_map = {}
 
     os.makedirs(IMAGE_DIR, exist_ok=True)
 
-    for i, page in enumerate(doc):
+    for i, page in enumerate(context):
         page_text = page.get_text()
         texts.append(page_text)
         images = []
 
         for img_index, img in enumerate(page.get_images(full=True)):
             xref = img[0]
-            base_image = doc.extract_image(xref)
+            base_image = context.extract_image(xref)
             img_bytes = base_image['image']
             img_ext = base_image['ext']
             img_obj = Image.open(io.BytesIO(img_bytes))
@@ -208,11 +217,6 @@ def load_pdf(pdf_path):
 # 벡터 스토어 구축
 # ----------------------
 def build_vectorstore(chunks, page_map):
-    docs = [
-        Document(page_content=chunk, metadata={"page": page_map[i]})
-        for i, chunk in enumerate(chunks)
-    ]
-
     embedding_model = HuggingFaceEmbeddings(model_name=EMBED_MODEL_NAME)
 
     if os.path.exists(VECTORSTORE_DIR):
@@ -221,12 +225,15 @@ def build_vectorstore(chunks, page_map):
             embedding_function=embedding_model
         )
     else:
+        context = [
+            Document(page_content=chunk, metadata={"page": page_map[i]})
+            for i, chunk in enumerate(chunks)
+        ]
         vectorstore = Chroma.from_documents(
-            docs,
+            context,
             embedding_model,
             persist_directory=VECTORSTORE_DIR
         )
-
 
     retriever = Chroma(
         persist_directory=VECTORSTORE_DIR,
@@ -252,74 +259,46 @@ def load_pipeline(model, tokenizer):
 
 def load_retrieval_qa_chain(text_gen, retriever):
     llm = HuggingFacePipeline(pipeline=text_gen)
-
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
         return_source_documents=True
     )
-
     return qa_chain
+
+def remove_duplicates_in_docs(docs):
+    source_docs = {}
+    for d in docs:
+        key = (d.metadata.get('source'), d.metadata.get('page'))
+        if key not in docs:
+            source_docs[key] = d
+    return list(source_docs.values())
 
 def generate_llm_answer(query, text_gen, retriever):
     qa_chain = load_retrieval_qa_chain(text_gen, retriever)
     answers = qa_chain.invoke({'query': query})
-    long_answer = answers['result'].split('Helpful Answer:')[-1].strip()
-    source_docs = answers.get('source_documents', [])
-    unique_docs = {}
-    for d in source_docs:
-        key = (d.metadata.get('source'), d.metadata.get('page'))
-        if key not in unique_docs:
-            unique_docs[key] = d
-    source_docs = list(unique_docs.values())
-    docs = [doc.page_content for doc in source_docs]
-    return long_answer, source_docs, docs
+    long_answer = answers['result'].split('Helpful Answer:')[-1].split('Answer:')[0].strip()
+    docs = answers.get('source_documents', [])
+    source_docs = remove_duplicates_in_docs(docs)
+    context = [doc.page_content for doc in source_docs]
+    return long_answer, source_docs, context
 
 def generate_pdf_answer(query, vectorstore, text_gen):
     prompt = PromptTemplate.from_template(TEMPLATE)
-    source_docs = vectorstore.similarity_search(query, k=3)
-    unique_docs = {}
-    for d in source_docs:
-        key = (d.metadata.get('source'), d.metadata.get('page'))
-        if key not in unique_docs:
-            unique_docs[key] = d
-    source_docs = list(unique_docs.values())
-    docs = [doc.page_content for doc in source_docs]
-    context = '\n'.join(docs)
+    docs = vectorstore.similarity_search(query, k=3)
+    source_docs = remove_duplicates_in_docs(docs)
+    context = [doc.page_content for doc in source_docs]
     formatted_prompt = prompt.format(context=context, query=query)
     output = text_gen(formatted_prompt, max_new_tokens=TOKEN_SIZE)[0]['generated_text']
     short_answer = output.split('답변:')[-1].strip()
-    return short_answer, source_docs, docs
+    return short_answer, source_docs, context
 
-def generate_summary(docs, text_gen):
-    prompt = PromptTemplate.from_template('다음 문서 내용을 바탕으로 요약해주세요.\n문서 내용:\n{context}\n\n요약:\n')
-    formatted_prompt = prompt.format(context=docs)
+def generate_summary(context, text_gen):
+    prompt = PromptTemplate.from_template(SUMMARY_TEMPLATE)
+    formatted_prompt = prompt.format(context=context)
     output = text_gen(formatted_prompt, max_new_tokens=TOKEN_SIZE)[0]['generated_text']
     summary = output.split('요약:')[-1].strip()
     return summary
-
-# ----------------------
-# UI
-# ----------------------
-def display_answer(answer, summary, docs, images):
-    with st.chat_message('assistant'):
-        st.markdown('### 답변')
-        st.markdown(answer)
-
-        if summary:
-            st.markdown('### 요약')
-            st.markdown(summary)
-
-        st.markdown('### 관련 문서')
-        for i, context in enumerate(docs, 1):
-            st.markdown(f'{i}. {context}')
-
-        if images:
-            st.markdown('### 관련 이미지')
-            cols = st.columns(3)
-            for i, img_path in enumerate(images):
-                with cols[i%3]:
-                    st.image(img_path, use_container_width=True)
 
 # ----------------------
 # langgraph
@@ -334,14 +313,14 @@ def build_graph(page, vectorstore, text_gen, retriever):
     def llm_answer(state):
         with st.spinner('LLM 답변 생성 중...'):
             query = state.get('query', '')
-            answer, source_docs, docs = generate_llm_answer(query, text_gen, retriever)
-        return {**state, 'answer': answer, 'source_docs': source_docs, 'docs': docs}
+            answer, source_docs, context = generate_llm_answer(query, text_gen, retriever)
+        return {**state, 'answer': answer, 'source_docs': source_docs, 'context': context}
 
     def pdf_answer(state):
         with st.spinner('ChromaDB 검색으로 답변 생성 중...'):
             query = state.get('query', '')
-            answer, source_docs, docs = generate_pdf_answer(query, vectorstore, text_gen)
-        return {**state, 'answer': answer, 'source_docs': source_docs, 'docs': docs}
+            answer, source_docs, context = generate_pdf_answer(query, vectorstore, text_gen)
+        return {**state, 'answer': answer, 'source_docs': source_docs, 'context': context}
 
     def next_step(state):
         return {**state}
@@ -353,8 +332,8 @@ def build_graph(page, vectorstore, text_gen, retriever):
         return 'get_summary' if answer_len > TOKEN_SIZE else END
 
     def get_summary(state):
-        docs = state.get('docs', '')
-        summary = generate_summary(docs, text_gen)
+        context = state.get('context', '')
+        summary = generate_summary(context, text_gen)
         return {**state, 'summary': summary}
 
     graph = StateGraph(dict)
@@ -382,6 +361,44 @@ def build_graph(page, vectorstore, text_gen, retriever):
     graph.add_edge('get_summary', END)
 
     return graph.compile()
+
+# ----------------------
+# UI
+# ----------------------
+def display_answer(answer, summary, context, images):
+    with st.chat_message('assistant'):
+        st.markdown('### 답변')
+        st.markdown(answer)
+
+        if summary:
+            st.markdown('### 요약')
+            st.markdown(summary)
+
+        st.markdown('### 관련 문서')
+        for i, docs in enumerate(context, 1):
+            st.markdown(f'{i}. {docs}')
+
+        if images:
+            st.markdown('### 관련 이미지')
+            cols = st.columns(3)
+            for i, img_path in enumerate(images):
+                with cols[i%3]:
+                    st.image(img_path, use_container_width=True)
+
+def get_content(output, page_images_map):
+    answer = output['answer']
+    summary = output.get('summary', '')
+    context = output.get('context', [])
+    if 'images' in output:
+        images = output.get('images', [])
+    else:
+        source_docs = output.get('source_docs', [])
+        images = []
+        for doc in source_docs:
+            page_idx = doc.metadata.get('page')
+            if page_idx is not None:
+                images.extend(page_images_map.get(page_idx, []))
+    return answer, summary, context, images
 
 # ----------------------
 # main
@@ -419,7 +436,8 @@ def main():
         with st.spinner('LoRA fine-tuning 기반 차량 점검 중...'):
             texts = load_csv(MY_CAR_STATUS)
             chunks, _ = chunk_text(texts)
-            model = lora_fine_tuning(model, tokenizer, chunks)
+            base_model = AutoModelForCausalLM.from_pretrained(MODEL_ID)
+            model = lora_fine_tuning(base_model, tokenizer, chunks)
             st.write('차량 점검 완료')
 
     text_gen = load_pipeline(model, tokenizer)
@@ -433,13 +451,9 @@ def main():
             with st.chat_message('user'):
                 st.markdown(message['content'])
         else:
-            content = message['content']
-            display_answer(
-                content['answer'],
-                content.get('summary', ''),
-                content.get('docs', []),
-                content.get('images', [])
-            )
+            output = message['content']
+            answer, summary, context, images = get_content(output, page_images_map)
+            display_answer(answer, summary, context, images)
 
     query = st.chat_input('질문 입력')
     if query:
@@ -447,28 +461,19 @@ def main():
         with st.chat_message('user'):
             st.markdown(query)
 
-        with st.spinner('답변 생성 중...'):
-            results = app.invoke({'query': query})
-            answer = results['answer']
-            summary = results.get('summary', '')
-            source_docs = results.get('source_docs', [])
-            docs = results['docs']
-            images = []
-            for doc in source_docs:
-                page_idx = doc.metadata.get('page')
-                if page_idx is not None:
-                    images.extend(page_images_map.get(page_idx, []))
+        output = app.invoke({'query': query})
+        answer, summary, context, images = get_content(output, page_images_map)
 
         st.session_state.messages.append({
             'role': 'assistant',
             'content': {
                 'answer': answer,
                 'summary': summary,
-                'docs': docs,
+                'context': context,
                 'images': images
             }
         })
-        display_answer(answer, summary, docs, images)
+        display_answer(answer, summary, context, images)
 
 if __name__ == "__main__":
     main()
